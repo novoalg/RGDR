62b6f9772bc320c4987744dd672b0dc63aa0aafc
Lucas Novoa <novoalg@uwec.edu>
2017-02-06 16:47:40 -0600

    Mailer settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,11 +35,11 @@ Rails.application.configure do
   config.assets.compile = true
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
-          :address => "smtp.gmail.com",
+          :address => "smtp.sendgrid.net",
           :port => "587",
-          :domain => "gmail.com",
-          :user_name => "rgdrtemp@gmail.com",
-          :password => ENV["GMAILPW"],
+          :domain => "heroku.com",
+          :user_name => ENV["SENDGRID_USERNAME"],
+          :password => ENV["SENDGRID_PASSWORD"],
           :authentication => "login",
           :enable_starttls_auto => true
   }
