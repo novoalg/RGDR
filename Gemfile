d7837fa99654a1a71e41ac507444606d8df90259
Lucas Novoa <novoalg@uwec.edu>
2017-02-06 18:20:27 -0600

    Testing email templates

--- Gemfile
+++ Gemfile
@@ -49,6 +49,9 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use ActiveModel has_secure_password
  gem 'bcrypt', '~> 3.1.11'
 
+#mailer
+gem 'sendgrid-ruby'
+
 # Use Unicorn as the app server
 # gem 'unicorn'
 
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,30 @@
 class UserMailer < ApplicationMailer
     default from: "rgdrtemp@gmail.com"
+    require 'sendgrid-ruby'
+    include SendGrid
+    require 'json'
 
     def welcome_email(user)
         @user = user
-        mail(to: @user.email, subject: "Registration Confirmation - Real Good Dog Rescue")
+        mail = Mail.new
+        mail.from = Email.new(email: 'rgdrtemp@gmail.com')
+        mail.subject = 'Registration Confirmation - Real Good Dog Rescue'
+        mail.to = Email.new(email @user.email)
+        personalization = Personalization.new
+        personalization.substitutions = Substitution.new(key: "-fullname-", value: @user.full_name)
+        personalization.substitutions = Substitution.new(key: "-confirmlink-", value: "#{link_to "Confirm Email", { controller: "users", action: "confirm_email", confirm_token: @user.confirm_token }}")
+        personalization.substitutions = Substitution.new(key: "-profilesettings-", value: "#{link_to "Profile Settings", edit_user_path(@user)}")
+        mail.template_id = "1cf09bc0-4dcb-4015-8fc7-ac179e726f85"
+        mail.personalizations = personalization
+        sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+        begin
+            response = sg.client.mail._("send").post(request_body: mail.to_json)
+        rescue Exception => e
+            puts e.message
+        end
+        puts response.status_code
+        puts response.body
+        puts response.headers
     end 
 
     def adopt_email(form_params)
